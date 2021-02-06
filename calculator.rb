792c6b1a11e33e9aa73cafaf22ee0f5733544d42
Henry Velasquez <hvelas93@gmail.com>
2021-02-06 17:21:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 # Add your variables here
-first_number=2
+first_number=1
 
-second_number=1
+second_number=2
 
 sum=first_number+second_number
 
-difference=second_number-first_number
+difference=first_number-second_number
